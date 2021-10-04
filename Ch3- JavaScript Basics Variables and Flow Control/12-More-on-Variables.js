064394769a72d6cc640f5aa2669261df2b2c4b11
Karim Adel <ka565872@gmail.com>
2021-10-04 11:53:39 +0200

    variables rules

---
+++ Ch3- JavaScript Basics Variables and Flow Control/12-More-on-Variables.js
@@ -0,0 +1,19 @@
+// 1. You cant define a variables more than once
+let petName = 'Hel'
+// let petName = 'Spot'
+petName = 'spot'
+
+// console.log(petName) // That's error cant dublicate with let-keyWord
+// To solve that can declare name without keyword
+
+console.log(petName) // Spot
+
+// 2. There are rules related to variables names
+/**
+ * 3 isn't valid name
+ * name start with digits or $ or _
+ * A not a As js is case sensitives
+ */
+
+// 3. Variables name cannot be reserved kewords as let
+// let let = 5 //that's error
