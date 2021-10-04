7a7f4526f2065fa97e415a21b44cb68d2d53470c
Karim Adel <ka565872@gmail.com>
2021-10-04 10:28:00 +0200

    how strings work

---
+++ Ch3- JavaScript Basics Variables and Flow Control/02-Strings-and-variables.js
@@ -0,0 +1,25 @@
+/* we used let as reversed keyword to store data */
+/* Syntax : [ let nameVar (=) value ]  */
+
+/**
+ * variable to store a name
+ */
+let myName = 'karim'
+
+/**
+ * variable to store a Sentence
+ */
+let sen = 'student at auc'
+
+/**
+ * console.log() => is used to print
+ */
+console.log(myName)
+
+let firstName = 'Karim'
+let lastName ='Adel'
+/* We can use plus operator to combine between two words or more */
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+/* Semicolons is optional in js and prfer use it in the end of file */
---
+++ Ch3- JavaScript Basics Variables and Flow Control/02-challenge(1).js
@@ -0,0 +1,10 @@
+/**
+ * Challenge setup
+ * Creat 3 var. => city, country, location
+ * and get location from combine city and cairo
+ */
+let city = 'Cairo'
+let country = 'Eygpt'
+
+let location = city + ', ' + country
+console.log(location)
