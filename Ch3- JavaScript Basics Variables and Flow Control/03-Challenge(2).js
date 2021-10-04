5f62026a3f281bd4a77a3ff390968f79a41b3434
Karim Adel <ka565872@gmail.com>
2021-10-04 11:08:41 +0200

    make calculater to get parcent of score

---
+++ Ch3- JavaScript Basics Variables and Flow Control/03-Challenge(2).js
@@ -0,0 +1,10 @@
+/**
+ * Challenge
+ * create two var. => studentScore, maxScore and parcent
+ */
+let studentScore = 8
+let maxScore = 10
+let parcent = (studentScore * 100) / maxScore
+
+// print parcent of 8 score from 10
+console.log(parcent)
