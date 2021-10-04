7e9f1c8f1efc505275480e3a49a62a02aee570f0
Karim Adel <ka565872@gmail.com>
2021-10-04 13:09:48 +0200

    upload file

---
+++ Ch3- JavaScript Basics Variables and Flow Control/15-challenge.js
@@ -0,0 +1,13 @@
+let age = 65
+/**
+ * If 7 or under print message about child pricing 
+ * if 65 or older print message about senior discount
+ */
+age = 6
+if (age <= 7) {
+    console.log('You will get a child discount!')
+}
+age = 69
+if (age >= 65) {
+    console.log('You will get a senior discount!')
+}
