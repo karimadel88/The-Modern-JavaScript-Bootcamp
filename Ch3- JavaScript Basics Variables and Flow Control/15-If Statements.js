132309cefe65df6c5c1e0b6b61a36d394e513124
Karim Adel <ka565872@gmail.com>
2021-10-04 13:04:24 +0200

    upload file

---
+++ Ch3- JavaScript Basics Variables and Flow Control/15-If Statements.js
@@ -0,0 +1,15 @@
+// let temp = 31
+// let isFreezing = temp >= 31
+// if (isFreezing) {
+//     console.log('It is freezing outside!')
+// } // will print 'It is freezing outside!' As condition is true
+
+/* Program flow */
+let temp = 31
+if(temp<=31){
+    console.log('It is freezing outside!')
+}
+temp = 110
+if(temp>=100){
+    console.log('It is way to hot outside!')
+}
