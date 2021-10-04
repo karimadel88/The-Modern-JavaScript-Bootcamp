ada3e63813cd8804621a9bc6e7dc4fc5cfc79572
Karim Adel <ka565872@gmail.com>
2021-10-04 10:50:40 +0200

    numbers in js

---
+++ Ch3- JavaScript Basics Variables and Flow Control/03-Numbers.js
@@ -0,0 +1,31 @@
+/* numbers is flexable can be positive or negtaive or .. */
+let n1 = 0.5
+let n2 = -5
+let n3 = 5
+
+/* Arithmatic operations */
+let num = 5.5
+/* Add */
+let num2 = num + 2
+/* Subtract */ 
+let num3 = num - 2
+/* Multiply */
+let num4 = 5 * 2
+/* Divide */
+let num5 = 6 / 2
+let num6 = 5 / 2
+
+/* start print */
+console.log(num)    // will print 5.5 
+console.log(num2)   // will print 7.5
+console.log(num3)   // will print 3.5
+console.log(num4)   // will print 10
+console.log(num5)   // will print 3
+console.log(num6)   // will print 2.5
+/* end print */
+
+
+let x = 11
+let num7 = x + 1 * 2 // 11 + 1 * 2 = 13
+// by arrange number with parentheses
+let num7 = (x+1) * 2 // 12 *2 = 24
