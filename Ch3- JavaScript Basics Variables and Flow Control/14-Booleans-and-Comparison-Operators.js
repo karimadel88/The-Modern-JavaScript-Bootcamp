3aa9dac7089a8ab2170d9e84de1f30099ea1ff81
Karim Adel <ka565872@gmail.com>
2021-10-04 12:42:30 +0200

    Booleans and Comparison operators

---
+++ Ch3- JavaScript Basics Variables and Flow Control/14-Booleans-and-Comparison-Operators.js
@@ -0,0 +1,29 @@
+// Boolean Data Type
+
+/* (===) => is equality operator
+   (!==) => no equal operator
+*/
+let temp = 31
+// let isFreezing = true // or false
+let isFreezing = temp === 31
+console.log(isFreezing) // will print true
+
+isFreezing = temp == 32 
+console.log(isFreezing) // will print false 
+
+isFreezing = temp!==32 // will prinr true
+
+
+let name = "karim"
+let isAhmed = name === 'ahmed'
+console.log(isAhmed) //will print false
+
+/**
+ * greater than >
+ * less than <
+ * greater than or equal to >=
+ * less than or equal to <=
+ */
+let isFreezing = temp > 32
+console.log(isFreezing)     // will print false
+let isFreezing = temp < 32  // will print true
