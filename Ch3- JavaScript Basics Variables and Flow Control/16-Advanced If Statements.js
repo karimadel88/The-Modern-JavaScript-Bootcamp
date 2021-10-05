2c4b4dfa85b1f2d7079d925e4b13db3cd6883f09
Karim Adel <ka565872@gmail.com>
2021-10-05 21:28:29 +0200

    More on if-statements

---
+++ Ch3- JavaScript Basics Variables and Flow Control/16-Advanced If Statements.js
@@ -0,0 +1,18 @@
+let isAccountLocked = true
+let userRole = 'karim'
+
+if (isAccountLocked) {
+    console.log('Account Is Locked')
+}else{
+    console.log('Welcome! ...')
+}
+
+if (isAccountLocked) {
+    console.log('Is account locked')
+}
+else if (userRole === 'karim') {
+    console.log('Welcome karim')
+}
+else {
+    console.log('Welcome! ...')
+}
