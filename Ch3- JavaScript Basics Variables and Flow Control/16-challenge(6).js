9973b7e1bfb62492e4f1f841536ddcb8b3aa28c9
Karim Adel <ka565872@gmail.com>
2021-10-05 21:32:58 +0200

    challenge about if-else

---
+++ Ch3- JavaScript Basics Variables and Flow Control/16-challenge(6).js
@@ -0,0 +1,17 @@
+/**
+ * 1- It is freezing outside!
+ * 2- It is not outside!
+ * 3- Go for it is pretty nice.
+ * 
+ */
+let temp = 36
+
+if (temp <= 32) {
+    console.log('It is freezing outside!')
+}
+else if (temp >= 40){
+    console.log('It is not outside!')
+}
+else{
+    console.log('Go for it is pretty nice.')
+}
