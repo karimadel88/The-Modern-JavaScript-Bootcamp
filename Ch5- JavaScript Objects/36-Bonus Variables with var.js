20a7dc1adaf38f1a6051322f316871c7c0afc834
Karim Adel <ka565872@gmail.com>
2021-10-14 19:52:24 +0200

    bonus file

---
+++ Ch5- JavaScript Objects/36-Bonus Variables with var.js
@@ -0,0 +1,30 @@
+
+var firstName = 'karim'
+// can reassign
+firstName = 'amgd'
+
+// can redecler
+var firstName = 'kamel'
+console.log(firstName)
+
+// var has a function based scope
+if (10 == 10) {
+    var secondName = 'Adel'
+}
+console.log(secondName)
+
+// const setName = function () {
+//     var thirdName = 'Eid' 
+// }
+// console.log(thirdName)
+
+var age // let or var have same value => undefined
+console.log(age)
+
+console.log(x)
+// let x = 5 // error
+var x = 5 // print undefined
+
+y = 17
+console.log(y) // print 17
+var y
